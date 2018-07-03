89c9fbca24a49eee65fb29c283b4882f083dfdf5
Jared Beck <jared@jaredbeck.com>
2018-07-03 15:27:26 -0400

    Show cars as a table

--- lib/http_server.rb
+++ lib/http_server.rb
@@ -5,46 +5,57 @@ require "rubygems"
 require "bundler/setup"
 require "mysql2"
 
-# Executes report.rb, producing no output,
-# after which, report_all func will be defined.
-require "/Users/terrell/Code/http_server/lib/report.rb"
-client = Mysql2::Client.new(:host => "localhost", :username => "root", :database => "inquiry")
-results = client.query("SELECT * FROM cars where name='Ford Focus ST'")
+db = Mysql2::Client.new(:host => "localhost", :username => "root", :database => "inquiry")
 
 def plain_result(results)
-  results.to_a.to_s
+  results.to_a.reduce('') do |a, record|
+    a << "<tr><td>#{record['name']}</td></tr>"
+  end
 end
 
+def query(client)
+  results = client.query("SELECT * FROM cars;")
+  plain_result(results)
+end
 
-message = <<~MSG
-<!DOCTYPE html>
-<html>
-<head>
-  <link rel="stylesheet" type="text/css" href="css/http_server.css"
-</head>
-<body>
-  <h1>
-    #{plain_result(results)}
-  </h1>
-</body>
-<footer>
-</footer>
-</html>
-MSG
+def message(client)
+  <<~MSG
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <link rel="stylesheet" type="text/css" href="css/http_server.css"
+    </head>
+    <body>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          #{query(client)}
+        </tbody>
+      </table>
+    </body>
+    <footer>
+    </footer>
+    </html>
+  MSG
+end
 
 server = TCPServer.new 1034 # Server bind to port 1034
 loop do
   client = server.accept    # Wait for a client to connect
+  msg = message(db)
   client.puts <<~HTTP
     HTTP/1.1 200
     Date: #{Time.now}
     Content-Type: text/html
     Server: Terrell's Awesome Server
     Connection: close
-    Content-Length: #{message.length}
+    Content-Length: #{msg.length}
 
-    #{message}
+    #{msg}
   HTTP
-  sleep(5)
   client.close
 end
